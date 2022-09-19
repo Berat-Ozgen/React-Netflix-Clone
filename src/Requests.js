838db1c596ded10fa3ea672dafabc71459cff45b
berat ozgen <berat.ozgnn.01@gmail.com>
2022-09-19 23:25:52 +0300

    React-Netflix-Clone
    React-Netflix-Clone

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "d9b8b0e16bb5b39ca47b17702d4fd301";
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests
